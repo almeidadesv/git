76d0676ba018694a24d6e255933252e0d6613e5e
almeidadesv <alikaaalmeid@gmail.com>
2026-02-27 21:13:58 -0300

    adicionado soma de valores

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 console.log("Hello Git!")
+
+// alterado o valor de a para 50
+
 let a =50;
+
+// adicionado a variável b com valor de 25
+
+let b = 25
+
+let soma = a + b;
+
+console.log("A soma de a e b é: " + soma);
